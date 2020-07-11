52b9c69300a2a4c0679c66fa0bcef111b0ca4e63
matrahul <63643299+matrahul@users.noreply.github.com>
2020-07-12 00:02:31 +0530

    Update main.tf

--- vnet-hub-spoke-model/module/network/web-vnet/main.tf
+++ vnet-hub-spoke-model/module/network/web-vnet/main.tf
@@ -29,14 +29,3 @@ resource "azurerm_virtual_network" "web_vnet" {
      address_prefix   = "${var.address_prefix_5}"
   }
 
- /*
- resource "azurerm_virtual_network_peering" "vnet_peer_web" {
-  name                         = "${var.vnet_peering_name_web}"
-  resource_group_name          = "${var.resource_group_name}"
-  virtual_network_name         = "${var.virtual_network_name_web}" 
-  remote_virtual_network_id    = "${data.azurerm_virtual_network.hub_vnet.id}"
-  allow_virtual_network_access = true
-  allow_forwarded_traffic      = true
-  use_remote_gateways          = true
-}
-*/
